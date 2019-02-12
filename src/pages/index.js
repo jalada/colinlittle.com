542db897ed49e96d1c7ca4749edf6674da5d1946
David Somers <126989+jalada@users.noreply.github.com>
2019-02-12 21:23:34 +0000

    Add link

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,14 @@ const IndexPage = () => (
   <Container hasTextAlign="centered">
     <iframe src="https://giphy.com/embed/3bgFvn00K0zW8" width="480" height="188" style={{maxWidth: '100%'}} frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
     <Title isSize="4" style={{paddingTop: '1rem'}}>
-      Thank you for your interest, but Colin is no longer providing consultancy
+      Thank you, but Colin is no longer providing consultancy
       services.
+    </Title>
+
+    <Title isSize="5">
+      <a style={{textDecoration: 'underline'}} href="https://www.colinlittlemusic.com">
+        Interested in Colin's music? Click here.
+      </a>
     </Title>
   </Container>
 )
